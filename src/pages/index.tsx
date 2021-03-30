d324623236aaeefac826928d69c5b0f0cbd19f28
Paulo Guilherme <paulopgss@gmail.com>
2021-03-30 20:42:25 -0300

    Set do estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
-}
+  return (
+    <Fragment>
+      <Head>
+        <title>
+          Inicio | ig.news
+        </title>
+      </Head>
+
+      <h1>Hello world!</h1>
+    </Fragment>
+  );
+};
